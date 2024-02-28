a0c065c5a06776d613277a8fd7d77f8c723fe2eb
Alejandro Ohrnialian Bengolea <104745560+AOB1992@users.noreply.github.com>
2024-02-27 21:54:14 -0300

    Update mercadopago.js

--- src/routes/mercadopago.js
+++ src/routes/mercadopago.js
@@ -4,7 +4,7 @@ const mercadopago = require("mercadopago");
 const router = Router();
 require("dotenv").config();
 MERCADOPAGO_KEY = "TEST-7243261396680960-020318-90f04e08ecb5b9150bffd30a7efb7eb5-214413477"
-mercadopago.configure({ access_token: process.env.MERCADOPAGO_KEY });
+mercadopago.configure({ access_token: MERCADOPAGO_KEY });
 console.log("2entrando a mercadopago.js")
 router.post("/", (req, res) => {
   const prod = req.body;
